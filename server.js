020baf3c022b647090fa6b6e735ca0ed1a0fd545
Alexis Brownstein <alexis@drdatabase.com>
2022-12-13 22:14:51 -0500

    added _deprecated folder to finalize mvc folder structure

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 //const bodyParser = require('body-parser');
-
+   
 //file imports
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
