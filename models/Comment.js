381125002e6e8d85d7827b3943eebbd1249c91c3
Alexis Brownstein <alexis@drdatabase.com>
2023-01-03 16:50:10 -0500

    created Comment.js model

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      //note: default STRING contains 255 char max
+      type: DataTypes.STRING,  
+    },
+    blog_id: {  //blog post
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
+    },   
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
