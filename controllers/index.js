5349ac61ba7ca3cd5f67936293b25cd126d84e83
Alexis Brownstein <alexis@drdatabase.com>
2023-01-04 13:25:57 -0500

    changed homeRoutes to viewRoutes throughout code, updated viewRoutes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+//viewRoutes means handlebar views
+const viewRoutes = require('./viewRoutes');
 
-router.use('/', homeRoutes);
+router.use('/', viewRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
